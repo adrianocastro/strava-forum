9e7c67f0fa8229da50c1dbcf9f336d04d0da9aa2
Adriano Castro <ad@adrianocastro.net>
2014-05-26 01:42:51 -0700

    First pass at thread page

--- routes/post.js
+++ routes/post.js
@@ -1,6 +1,12 @@
 exports.thread = function (req, res) {
     var post = require('../data/post.json');
 
+    var threads = post.post.threads;
+
+    for(var i in threads){
+        threads[i].content = threads[i].content.replace(/\n/gi,'<br><br>')
+    }
+
     var title = post.post.title + ' - Strava Forum';
 
     res.render('post', {
